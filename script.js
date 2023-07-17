c75ce9ef6944bacb899cb429d1d5253f73826e74
Satishkumarvaradaboina <117851826+Satishkumarvaradaboina@users.noreply.github.com>
2023-07-17 07:49:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+//your CSS code here. If required.
+ 
+ 
+#line {
+  width: 200px;
+  height: 2px;
+  background-color: black;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+ 
+ 
+//your JS code here. If required.
+ 
+ 
+const line = document.getElementById('line');
+let rotation = 0;
+ 
+setInterval(() => {
+  rotation += 5;
+  line.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
+}, 50);
